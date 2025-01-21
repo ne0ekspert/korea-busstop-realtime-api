ec0afd8fbfd6e2302d8e4bc2c271c1381cc8663d
ne0ekspert <rlaaudwk5503@gmail.com>
2025-01-21 22:51:40 +0900

    Weather viewer

--- src/app/utils/datagokrRequest.ts
+++ src/app/utils/datagokrRequest.ts
@@ -122,8 +122,14 @@ export async function getNearStations(lat: number, long: number): Promise<getNea
 }
 
 interface weatherForecastItem {
-  regId: string;
-  rnSt5Am: number
+  baseDate: `${number}`;
+  baseTime: "0200" | "0500" | "0800" | "1100" | "1400" | "1700" | "2000" | "2300";
+  category: "POP" | "PTY" | "PCP" | "REH" | "SNO" | "SKY" | "TMP" | "TMN" | "TMX" | "UUU" | "VVV" | "WAV";
+  fcstDate: `${number}`;
+  fcstTIme: "0000" | "0100" | "0200" | "0300" | "0400" | "0500" | "0600" | "0700" | "0800" | "0900" | "1000" | "1100" | "1200" | "1300" | "1400" | "1500" | "1600" | "1700" | "1800" | "1900" | "2000" | "2100" | "2200" | "2300";
+  fcstValue: string;
+  nx: number;
+  ny: number;
 }
 
 interface getWeatherForecastResponse {
@@ -141,22 +147,42 @@ interface getWeatherForecastResponse {
   }
 }
 
-export async function getWeatherForecast(lat: number, long: number): Promise<getWeatherForecastResponse> {
+export interface getWeatherForecastResult {
+  [date: string]: {
+    [category: string]: string
+  }
+}
+
+export async function getWeatherForecast(lat: number, long: number): Promise<getWeatherForecastResult> {
   const cache = new LRUCache({
     max: 50,
     ttl: 60 * 60 * 1000, // 1 hour TTL
   });
 
+  lat = Math.round(lat);
+  long = Math.round(long);
+
   const now = new Date();
   let hour = now.getHours();
   if (now.getMinutes() < 10) hour--;
+  const availableDataHours = [
+    2, 5, 8, 11, 14, 17, 20, 23
+  ].filter((v) => v - hour <= 0);
+  hour = Math.max(...availableDataHours);
+  const padHour = hour.toString().padStart(2,'0');
+  const padMonth = (now.getMonth()+1).toString().padStart(2, '0');
+  const padDate = now.getDate().toString().padStart(2, '0');
 
-  const paddedHours = `${hour}`.padStart(2, '0')+'00';
-  const cacheKey = `forecast-${paddedHours}`;
+  const baseDate = `${now.getFullYear()}${padMonth}${padDate}`;
+  const baseTime = `${padHour}00`;
+  const cacheKey = `forecast-${baseDate}-${baseTime}`;
 
-  if (cache.has(cacheKey)) {
-    return cache.get(cacheKey) as getWeatherForecastResponse;
-  }
+  //if (cache.has(cacheKey)) {
+  //  return cache.get(cacheKey) as getWeatherForecastResult;
+  //}
+
+  console.log('baseDate:', baseDate);
+  console.log('baseTime:', baseTime);
 
   const request = await axios.get(
     "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst",
@@ -164,14 +190,31 @@ export async function getWeatherForecast(lat: number, long: number): Promise<get
       params: {
         serviceKey: process.env.DATAGOKR_KEY,
         nx: lat,
-        ny: long
+        ny: long,
+        base_date: baseDate,
+        base_time: baseTime,
+        pageNo: 1,
+        numOfRows: 1000,
+        dataType: 'JSON'
       }
     }
   );
 
   const data: getWeatherForecastResponse = request.data;
+  
+  console.log(data);
+  const result: getWeatherForecastResult = {};
+  for (const item of data.response.body.items.item) {
+    if (!result.hasOwnProperty(item.fcstDate)) {
+      result[item.fcstDate] = {
+        [item.category]: item.fcstValue
+      };
+    } else {
+      result[item.fcstDate][item.category] = item.fcstValue;
+    }
+  }
 
-  cache.set(cacheKey, data);
+  cache.set(cacheKey, result);
 
-  return data;
+  return result;
 }
